2a80e9f32a523254ad629340b963cfc6e7ff7118
alexsenga <alexxngabo@gmail.com>
2022-07-27 13:07:01 +0200

    feat: install and configure react-native-vector-icons

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,10 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 
 const Home = () => {
   return (
     <View>
       <Text>Home</Text>
+      <Icon name='rocket' size={40} color={'blue'} />
     </View>
   )
 }
